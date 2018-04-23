3b35f37794fd1e59fa41f851c63f86c69fdee527
Cam Jackson <camjackson89@gmail.com>
2018-04-23 09:19:53 +0800

    Wrap the rendering in a global function that has to be called by the containing page

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('container'));
-registerServiceWorker();
+window.renderBrowseMicrofrontend = containerId => {
+  ReactDOM.render(<App />, document.getElementById(containerId));
+  registerServiceWorker();
+};
